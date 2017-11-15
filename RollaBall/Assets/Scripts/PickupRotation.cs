521cfa4ede04ba83f0a11ca7c991da2c0c2357af
Mat Solomon <matsolomon91@gmail.com>
2017-11-15 11:08:12 -0500

    RollaBall

---
+++ RollaBall/Assets/Scripts/PickupRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupRotation : MonoBehaviour
+{
+    public float RotateSpeed;
+    private Vector3 RotateVector;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 RotateVector = new Vector3(RotateSpeed*150, RotateSpeed*150, -RotateSpeed*100);
+        transform.Rotate(RotateVector * Time.deltaTime, Space.World);
+    }
+}
---
+++ RollaBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private int count;
+    public Text countText;
+
+    public AudioSource pickupSFX;
+
+    public float speed;
+    private Rigidbody rb;
+    private int iThrust = 1;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        if (iThrust < 2)
+        {
+            rb.AddForce(movement * 20 * speed);
+            iThrust++;
+        }
+        else
+        {
+            rb.AddForce(movement * speed);
+            iThrust = 1;
+        }
+        
+
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            //AudioSource audio = other.GetComponent<AudioSource>();
+            //audio.Play();
+            pickupSFX.Play();
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
+}
